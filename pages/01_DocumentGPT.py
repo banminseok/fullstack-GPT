21ca477d5771fe132bbe9a403081dac76a02fcb2
banminseok <halfmin@gmail.com>
2025-11-12 21:25:09 +0900

    7.7 chat history

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -12,6 +12,10 @@ st.set_page_config(
     page_icon="📃",
 )
 
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+@st.cache_data(show_spinner="Embedding file...")
 def embed_file(file):
     file_content = file.read()
     file_path = f"./.cache/files/{file.name}"
@@ -25,13 +29,24 @@ def embed_file(file):
         chunk_size=500,
         chunk_overlap=100,
     )
-    loader = UnstructuredFileLoader("./files/1984-orwell.txt")
+    loader = UnstructuredFileLoader(file_path)
     docs = loader.load_and_split(text_splitter=splitter)
     embeddings = OpenAIEmbeddings()
     cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
     vectorstore = FAISS.from_documents(docs, cached_embeddings)
     retriever = vectorstore.as_retriever()
     return retriever
+
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        st.session_state["messages"].append({"role": role, "message": message})
+
+
+def paint_history():
+    for chat in st.session_state["messages"]:
+        send_message(chat["message"], chat["role"], save=False)
 
 st.title("DocumentGPT")
 
@@ -43,12 +58,21 @@ Use this chatbot to ask questions to an AI about your files!
 """
 )
 
-file = st.file_uploader(
-    "Upload a .txt .pdf or .docx file",
-    type=["pdf", "txt", "docx"],
-)
+with st.sidebar:
+    file = st.file_uploader(
+        "Upload a .txt .pdf or .docx file",
+        type=["pdf", "txt", "docx"],
+    )
 
 if file:
     retriever = embed_file(file)
-    s = retriever.invoke("winston")
-    s
+    
+    send_message("I'm ready! Ask away!", "ai", save=False)
+    paint_history()
+    message = st.chat_input("Ask anything about your file...")
+    
+    if message:
+        send_message(message, "human")
+
+        
+    
